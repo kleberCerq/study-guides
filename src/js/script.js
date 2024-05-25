7e05e90b64bce9203a48680b98f885155fa5c622
kleberCerq <klebercerqueira07@gmail.com>
2024-05-25 13:23:14 -0300

    Sistema para adicionar novo guia criado
    Adicionado o botão para exibir o formulário de criação de novo guia e também o próprio formulário.
    Cor de fundo da página definida.

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+var formContainer = document.getElementById("form-container");
+
+function displayForm() {
+  formContainer.style.display = "flex";
+}
+
+function hideForm() {
+  formContainer.style.display = "none";
+}
