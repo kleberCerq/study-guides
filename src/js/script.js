cb3e88e9fd6c3330087bbb1280a462891b3fded1
kleberCerq <klebercerqueira07@gmail.com>
2024-05-31 22:40:10 -0300

    Sistema de botão dos guias adicionados
    Adicionada a função JavaScript para adicionar o botão do guia na tela.
    Adicionado o estilo para os botões de guia.
    Adicionado o documento HTML para guardar os guias.

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,16 @@ function displayForm() {
 function hideForm() {
   formContainer.style.display = "none";
 }
+
+function createGuide() {
+  var guideTitle = document.getElementById("input-title").value;
+  if (guideTitle) {
+    var guideLink = document.createElement("a");
+    guideLink.className = "guide-link";
+    guideLink.href = "guides.html"
+    guideLink.textContent = guideTitle;
+
+    document.getElementById("guide-container").appendChild(guideLink);
+    hideForm();
+  }
+}
